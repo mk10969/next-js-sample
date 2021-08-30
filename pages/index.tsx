8c516a8bdad7554632f6f66fc4182de66a3d7ddb
mk10969 <mk10969@gmail.com>
2021-08-30 18:11:57 +0900

    ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,8 @@
 import ApplianceList from '../components/content';
 // import GridContent from '../components/grid-layout/content';
 // import { GetStaticProps } from 'next';
-import { Appliance } from '../components/appliance';
 
-interface HomeProps {
-  appliances: Appliance[];
-}
-
-export default function Home(_props: HomeProps): JSX.Element {
+export default function Home(): JSX.Element {
   return (
     <>
       {/* <ApplianceList />
